1b7d17465e5c126599ae8898705bb16de688af79
Remy Phan <phan.remy@gmail.com>
2019-12-10 14:48:38 +0100

    mapbox working and customized

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,6 +9,16 @@ class PagesController < ApplicationController
   end
 
   def travelogues
+    @cities = City.geocoded
+
+    @markers = @cities.map do |city|
+      {
+        lat: city.latitude,
+        lng: city.longitude,
+        infoWindow: render_to_string(partial: "cities/info_window", locals: { city: city }),
+        image_url: helpers.asset_url('components/locationflag.png')
+      }
+    end
   end
 
   def shop
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,7 @@
 const { environment } = require('@rails/webpacker')
 
 const webpack = require('webpack')
+environment.loaders.delete('nodeModules');
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery',
